b78eec8942517c896632165ac5f866b71ec05a97
shangria <shangrialena@gmail.ru>
2021-01-04 00:01:41 +0200

    slider-owners

--- src/js/slider-owners.js
+++ src/js/slider-owners.js
@@ -1,4 +1,31 @@
-window.addEventListener('load', function (){
+window.addEventListener('load', function () {
+    debugger
 
+    const ownersBlock = document.getElementById('local-shop-owner_slider');
+    const liArray = [...ownersBlock.querySelectorAll('li')];
+    let currentIndex;
 
-})
+    function toggleSlider() {
+        let nextIndex;
+        if (currentIndex == null) {
+            nextIndex = 0;
+        } else if (currentIndex === liArray.length - 1) {
+            nextIndex = 0;
+        } else {
+            nextIndex = currentIndex + 1;
+        }
+
+        liArray.forEach(function (element, index) {
+            if (index === nextIndex) {
+                element.classList.remove('local-shop-owner_hidden');
+            } else {
+                element.classList.add('local-shop-owner_hidden');
+            }
+        });
+
+        currentIndex = nextIndex;
+    }
+
+    toggleSlider();
+    setInterval(toggleSlider, 2000);
+});
