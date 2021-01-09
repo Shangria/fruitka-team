4df8840e16cbeb5a23f5317cbb82068d81081d0a
shangria <shangrialena@gmail.ru>
2021-01-09 12:57:22 +0200

    hidden menu navigation

--- src/js/header-sub-menu.js
+++ src/js/header-sub-menu.js
@@ -1,27 +1,17 @@
 window.addEventListener('load', function () {
-    debugger
+
     const headerTitle = document.getElementById('header__menu-title')
     const itemTitle = headerTitle.querySelectorAll('[data-sub-menu]');
 
     itemTitle.forEach(function (element) {
-        element.addEventListener('mouseover', showElement,)
-        element.addEventListener('mousemove', hiddenElement,)
+        element.addEventListener('mouseenter', showAndHiddenElement, false);
+        element.addEventListener('mouseleave', showAndHiddenElement, false);
     })
 
-    function showElement() {
-        debugger
+    function showAndHiddenElement() {
         if (this.children.length > 1) {
-            this.children[1].classList.add('header_sub-menu-active')
+            this.children[1].classList.toggle('header_sub-menu-active')
         } else {
-            return false
-        }
-    }
-
-    function hiddenElement() {
-        if (this.children.length > 1) {
-            this.children[1].classList.remove('header_sub-menu-active')
-        }
-        else {
             return false
         }
     }
