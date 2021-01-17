8e1186344526c160850e715bebd90a00a660e9fa
shangria <shangrialena@gmail.ru>
2021-01-18 00:59:36 +0200

    sliders

--- src/js/banner-slider.js
+++ src/js/banner-slider.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', function () {
 
-    const sliderBanner = new Slider(document.querySelectorAll('[data-slider-banner]'), 5000);
+    const sliderBanner = new Slider(document.querySelectorAll('[data-slider-banner]'), 4000);
     const arrowLeft = document.getElementById('data-slider-left');
     const arrowRight = document.getElementById('data-slider-right');
 
