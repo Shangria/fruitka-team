2e705c7fe25328becbaa09afc48a34f3e3192289
shangria <shangrialena@gmail.ru>
2021-01-19 01:18:18 +0200

    sliders

--- src/js/banner-slider.js
+++ src/js/banner-slider.js
@@ -1,20 +1,17 @@
 window.addEventListener('load', function () {
 
-    const sliderBox = new Slider(document.querySelectorAll('[data-sliders-box]'), 4000);
-    // const sliderBanner = new Slider(document.querySelectorAll('[data-slider-banner]'), 4000);
+    const sliderBox = new Slider(document.querySelectorAll('[data-sliders-box]'), 777000);
     const arrowLeft = document.getElementById('data-slider-left');
     const arrowRight = document.getElementById('data-slider-right');
 
 
     arrowLeft.addEventListener('click', function () {
         sliderBox.prev();
-        // sliderBanner.prev();
     });
 
 
     arrowRight.addEventListener('click', function () {
         sliderBox.next()
-        // sliderBanner.next();
     });
 });
 
