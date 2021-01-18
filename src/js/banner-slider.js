a60e77aa91658185414b7128747407583f65d9fa
shangria <shangrialena@gmail.ru>
2021-01-19 00:49:28 +0200

    sliders

--- src/js/banner-slider.js
+++ src/js/banner-slider.js
@@ -1,17 +1,20 @@
 window.addEventListener('load', function () {
 
-    const sliderBanner = new Slider(document.querySelectorAll('[data-slider-banner]'), 4000);
+    const sliderBox = new Slider(document.querySelectorAll('[data-sliders-box]'), 4000);
+    // const sliderBanner = new Slider(document.querySelectorAll('[data-slider-banner]'), 4000);
     const arrowLeft = document.getElementById('data-slider-left');
     const arrowRight = document.getElementById('data-slider-right');
 
 
     arrowLeft.addEventListener('click', function () {
-        sliderBanner.prev();
+        sliderBox.prev();
+        // sliderBanner.prev();
     });
 
 
     arrowRight.addEventListener('click', function () {
-        sliderBanner.next();
+        sliderBox.next()
+        // sliderBanner.next();
     });
 });
 
