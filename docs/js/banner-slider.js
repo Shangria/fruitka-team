9361f920dad57316772b1615a7c8d45e17d7ff6e
shangria <shangrialena@gmail.ru>
2021-01-19 22:43:17 +0200

    slider

--- docs/js/banner-slider.js
+++ docs/js/banner-slider.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', function () {
 
     const sliderBox = new Slider(document.querySelectorAll('[data-sliders-box]'), 4000);
+    const slider = document.getElementById('wrapper-banner-slider');
     const arrowLeft = document.getElementById('data-slider-left');
     const arrowRight = document.getElementById('data-slider-right');
 
@@ -13,5 +14,35 @@ window.addEventListener('load', function () {
     arrowRight.addEventListener('click', function () {
         sliderBox.next()
     });
+
+    let initialPoint;
+    let finalPoint;
+
+    slider.addEventListener('touchstart', function (event) {
+
+        event.preventDefault();
+        event.stopPropagation();
+        initialPoint = event.changedTouches[0];
+    }, false);
+
+    document.addEventListener('touchend', function (event) {
+
+        event.preventDefault();
+        event.stopPropagation();
+debugger
+        finalPoint = event.changedTouches[0];
+        const xAbs = Math.abs(initialPoint.pageX - finalPoint.pageX);
+        const yAbs = Math.abs(initialPoint.pageY - finalPoint.pageY);
+        if (xAbs > 20 || yAbs > 20) {
+            if (xAbs > yAbs) {
+                if (finalPoint.pageX < initialPoint.pageX) {
+                    sliderBox.next();
+                } else {
+                    sliderBox.prev()
+                }
+            }
+        }
+    }, false);
+
 });
 
--- docs/js/slider-owners.js
+++ docs/js/slider-owners.js
@@ -1,3 +1,4 @@
 window.addEventListener('load', function () {
     const slider = new Slider(document.querySelectorAll('#local-shop-owner_slider li'), 3000);
+
 });
